8931263ca981255382bbce7486bb14767af6c7c3
Hugoberry <hugoberry314@gmail.com>
2021-01-19 15:59:01 +0000

    adding AG plot

--- nwb.config.js
+++ nwb.config.js
@@ -3,5 +3,11 @@ module.exports = {
   npm: {
     esModules: true,
     umd: false
-  }
+  },
+    devServer: {
+    port: 3000,
+    public: "localhost:3000",
+    publicPath: "http://localhost:3000/", 
+    disableHostCheck: true
+  },
 }
